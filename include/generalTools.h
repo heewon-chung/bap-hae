1d057566581cda415144107438889ccea0b5dbaa
Heewon Chung <runghyun@snu.ac.kr>
2017-08-22 00:33:56 +0900

    added some message generation function for various settings

--- include/generalTools.h
+++ include/generalTools.h
@@ -2,6 +2,10 @@
 #include <vector>
 #include <iostream>
 
+#include "param.h"
+
 using namespace std;
 
 void generateMsgAndTag(vector<int>&, vector<int>&, int);
+void generateMsgAndTag(vector<ZZ>&, vector<ZZ>&, int);
+void generateBinaryMsgAndTag(vector<int>&, vector<int>&, int);
--- src/generalTools.cpp
+++ src/generalTools.cpp
@@ -4,6 +4,24 @@ void generateMsgAndTag(vector<int>& msg, vector<int>& tag, int num){
     msg.clear(); tag.clear();
     
     for(unsigned long i = 0; i < num; i++){
+        msg.push_back(rand() % 10);
+        tag.push_back(rand() % 10);
+    }
+}
+
+void generateMsgAndTag(vector<ZZ>& msg, vector<ZZ>& tag, int num){
+    msg.clear(); tag.clear();
+    
+    for(unsigned long i = 0; i < num; i++){
+        msg.push_back(RandomBnd(MSGSPACE));
+        tag.push_back(RandomBnd(TAGSPACE));
+    }
+}
+
+void generateBinaryMsgAndTag(vector<int>& msg, vector<int>& tag, int num){
+    msg.clear(); tag.clear();
+    
+    for(unsigned long i = 0; i < num; i++){
         msg.push_back(rand() % 2);
         tag.push_back(rand() % 10);
     }
