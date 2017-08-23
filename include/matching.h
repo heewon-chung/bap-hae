9cc052a8ea0f65e8e7fa5de94439ffb22cdeb021
Heewon Chung <runghyun@snu.ac.kr>
2017-08-23 19:22:19 +0900

    add evaluation key for HAE

--- include/matching.h
+++ include/matching.h
@@ -8,6 +8,6 @@
 #include "hae.h"
 
 long hammingDistance(const vector<HAEPtxt>&, const vector<HAEPtxt>&);
-void hammingDistance(HAECtxt&, const vector<HAECtxt>&, const vector<HAECtxt>&);
+void hammingDistance(HAECtxt&, const vector<HAECtxt>&, const vector<HAECtxt>&, const HAEEvalKey&);
 
 #endif
