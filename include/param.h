f51ce2f3a5fb43b8a9c9b0dceb93f1790b5a1cbe
Heewon Chung <runghyun@snu.ac.kr>
2017-08-23 19:31:50 +0900

    parameter update for security level 80

--- include/param.h
+++ include/param.h
@@ -5,13 +5,13 @@
 
 using namespace NTL;
 
-const long SECURITY = 20;
-const long ERRORBND = 5;
-const long RANDOMBITS = SECURITY;
+const long  NUMBITS = 2048;
+const long  SECURITY = 80;
+const long  ERRORBND = 2 * SECURITY;
+const long  RANDOMBITS = 40;
+const long  NUMRANDOMIZER = 100;
 
-const ZZ MSGSPACE = conv<ZZ>((1 << SECURITY));
-const ZZ TAGSPACE = conv<ZZ>((1 << SECURITY));
-
-const long NUMRANDOMIZER = 10;
+const ZZ    MSGSPACE = conv<ZZ>(1) << 2 * SECURITY;
+const ZZ    TAGSPACE = conv<ZZ>(1) << 2 * SECURITY;
 
 #endif
