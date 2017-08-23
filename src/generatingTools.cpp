11e7da02cef0b365b419a7ffc8f62f096ed9c8dc
Heewon Chung <runghyun@snu.ac.kr>
2017-08-23 19:21:08 +0900

    change the function name

--- src/generatingTools.cpp
+++ src/generatingTools.cpp
@@ -5,8 +5,10 @@ void generateRandomPtxt(vector<HAEPtxt>& ptxt, int num){
     ptxt.resize(num);
     
     for(unsigned long i = 0; i < num; i++){
-        ptxt[i].msg = rand() % 10;
-        ptxt[i].tag = rand() % 10;
+        RandomBits(ptxt[i].msg, RANDOMBITS);
+        ptxt[i].msg %= MSGSPACE;
+        RandomBits(ptxt[i].tag, RANDOMBITS);
+        ptxt[i].tag %= TAGSPACE;
     }
 }
 
@@ -16,33 +18,32 @@ void generateBinaryMsg(vector<HAEPtxt>& ptxt, int num){
     
     for(unsigned long i = 0; i < num; i++){
         ptxt[i].msg = rand() % 2;
-        ptxt[i].tag = rand() % 10;
+        RandomBits(ptxt[i].tag, RANDOMBITS);
     }
 }
 
-void generatingRandomizer(vector<HAEPtxt>& rnd, vector<HAECtxt>& encRnd, const HAESecKey& secretKey){
+void generateRandomizer(vector<HAEPtxt>& rnd, vector<HAECtxt>& encRnd, const HAESecKey& secretKey){
     rnd.clear();
     rnd.resize(NUMRANDOMIZER);
 
     #pragma omp parallel for
     for(unsigned long i = 0; i < NUMRANDOMIZER; i++){
-        rnd[i].msg = RandomBits_ZZ(RANDOMBITS) % MSGSPACE;
-        rnd[i].tag = conv<ZZ>(1);
+        RandomBits(rnd[i].msg, RANDOMBITS);
+        rnd[i].msg %= MSGSPACE;
+        rnd[i].tag = conv<ZZ>(0);
     }
     encrypt(encRnd, rnd, secretKey);
 }
 
-void generateRandomSumOne(vector<ZZ>& rnd){
+void generateRandomVector(vector<ZZ>& rnd){
     rnd.clear();
     rnd.resize(NUMRANDOMIZER);
     
     ZZ sum;
 
-    for(unsigned long i = 0; i < NUMRANDOMIZER - 1; i++){
+    for(unsigned long i = 0; i < NUMRANDOMIZER; i++){
         RandomBits(rnd[i], RANDOMBITS);
         rnd[i] %= MSGSPACE;
         sum += rnd[i];
     }
-
-    rnd[NUMRANDOMIZER - 1] = 1 - sum;
 }
