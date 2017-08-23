875f06532c88a277b724487c3a19ce9e669b0be5
Heewon Chung <runghyun@snu.ac.kr>
2017-08-23 19:22:37 +0900

    change the time unit

--- src/timeUtils.cpp
+++ src/timeUtils.cpp
@@ -8,5 +8,5 @@ double get_time_us( std::chrono::time_point<std::chrono::steady_clock> & start,
 					std::chrono::time_point<std::chrono::steady_clock> & end, 
 					uint32_t N){
     auto diff = end-start;
-    return (long double)(std::chrono::duration_cast<std::chrono::seconds>(diff).count()) / N;
+    return (long double)(std::chrono::duration_cast<std::chrono::microseconds>(diff).count()) / N;
 }
